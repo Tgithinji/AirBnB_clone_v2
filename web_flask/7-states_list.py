293c55c3f9708be988c6a931ac11d264599f9ab8
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-10-23 00:00:21 +0300

    Display HTML page with a list of all states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+7-states_list module
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Displays a HTML page with a list of states
+    """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontent
+def teardown(self):
+    """Remove the current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
